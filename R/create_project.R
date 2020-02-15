bfce965d109da8f43bdb4aa35709537f38c22931
Ted Laderas <tedladeras@gmail.com>
2020-02-15 21:08:59 +0000

    updating documentation

--- R/create_project.R
+++ R/create_project.R
@@ -1,4 +1,16 @@
-create_ines_project <- function(destdir = ".") {
+#' Create a new lesson repo
+#'
+#' Use this function to start a brand new course based on the template available
+#' at https://github.com/ines/course-starter-r/
+#'
+#' @param destdir - destination directory for creating the course repository.
+#' It will be created as `course-starter-r`.
+#'
+#' @return new project as created by usethis::create_from_github
+#' @export
+#'
+#' @examples
+create_lesson_repo <- function(destdir = NULL) {
   usethis::create_from_github("ines/course-starter-r",destdir = destdir)
-
+  ui_done("Course is now created. Please rename your project folder when you have a chance.")
 }
